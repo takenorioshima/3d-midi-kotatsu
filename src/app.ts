468aca96c6c55ca5b4ec8f8f6d96c13cdbdc499e
Takenori Oshima <takenorioshima@gmail.com>
2024-05-09 11:37:58 +0900

    clean: Beautify

--- src/app.ts
+++ src/app.ts
@@ -4,46 +4,46 @@ import { Engine, Scene, SceneLoader, ArcRotateCamera, Vector3, HemisphericLight,
 import '@babylonjs/loaders/glTF';
 
 class App {
-    constructor() {
-        // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "main-canvas";
-        document.body.appendChild(canvas);
+  constructor() {
+    // create the canvas html element and attach it to the webpage
+    var canvas = document.createElement("canvas");
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.id = "main-canvas";
+    document.body.appendChild(canvas);
 
-        // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+    // initialize babylon scene and engine
+    var engine = new Engine(canvas, true);
+    var scene = new Scene(engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+    camera.attachControl(canvas, true);
+    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
-                } else {
-                    scene.debugLayer.show();
-                }
-            }
-        });
+    // hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+      // Shift+Ctrl+Alt+I
+      if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+        if (scene.debugLayer.isVisible()) {
+          scene.debugLayer.hide();
+        } else {
+          scene.debugLayer.show();
+        }
+      }
+    });
 
-        // Load model.
-        SceneLoader.ImportMeshAsync('', "./models/", "kotatsu.glb", scene)
-            .then((result) => {
-                const kotatsu = result.meshes[0];
-                kotatsu.scaling = new Vector3(0.4, 0.4, 0.4);
-            })
-            .catch(console.error);
+    // Load model.
+    SceneLoader.ImportMeshAsync('', "./models/", "kotatsu.glb", scene)
+      .then((result) => {
+        const kotatsu = result.meshes[0];
+        kotatsu.scaling = new Vector3(0.4, 0.4, 0.4);
+      })
+      .catch(console.error);
 
-        // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
-        });
-    }
+    // run the main render loop
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  }
 }
 new App();
