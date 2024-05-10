5195d29e32b0fcb01a66be57d4dcda2a08a2623f
Takenori Oshima <takenorioshima@gmail.com>
2024-05-10 11:03:15 +0900

    clean: Beautify

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,49 @@
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Color4 } from "@babylonjs/core";
+import '@babylonjs/core/Debug/debugLayer';
+import '@babylonjs/inspector';
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  Color4,
+} from '@babylonjs/core';
 import '@babylonjs/loaders/glTF';
 import Kotatsu from './kotatsu';
 
 class App {
   constructor() {
     // create the canvas html element and attach it to the webpage
-    var canvas = document.createElement("canvas");
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.id = "main-canvas";
+    var canvas = document.createElement('canvas');
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.id = 'main-canvas';
     document.body.appendChild(canvas);
 
     // initialize babylon scene and engine
     var engine = new Engine(canvas, true);
     var scene = new Scene(engine);
-    scene.clearColor = Color4.FromHexString("#ffcc00");
+    scene.clearColor = Color4.FromHexString('#ffcc00');
 
-    const camera = new ArcRotateCamera("camera", Math.PI / 2, Math.PI / 3, 6, Vector3.Zero(), scene);
+    const camera = new ArcRotateCamera(
+      'camera',
+      Math.PI / 2,
+      Math.PI / 3,
+      6,
+      Vector3.Zero(),
+      scene
+    );
     camera.attachControl(canvas, true);
-    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    var light1: HemisphericLight = new HemisphericLight(
+      'light1',
+      new Vector3(1, 1, 0),
+      scene
+    );
 
     // hide/show the Inspector
-    window.addEventListener("keydown", (ev) => {
+    window.addEventListener('keydown', (ev) => {
       // Shift+Ctrl+Alt+I
-      if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.code === "KeyI") {
+      if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.code === 'KeyI') {
         if (scene.debugLayer.isVisible()) {
           scene.debugLayer.hide();
         } else {
@@ -38,7 +56,7 @@ class App {
     const kotatsu = new Kotatsu(scene);
 
     // Keep aspect ratio on window resize.
-    window.addEventListener("resize", function () {
+    window.addEventListener('resize', function () {
       engine.resize();
     });
 
--- src/kotatsu.ts
+++ src/kotatsu.ts
@@ -1,7 +1,15 @@
-import { AbstractMesh, Color3, PointLight, Scene, SceneLoader, StandardMaterial, TransformNode, Vector3 } from "@babylonjs/core";
+import {
+  AbstractMesh,
+  Color3,
+  PointLight,
+  Scene,
+  SceneLoader,
+  StandardMaterial,
+  TransformNode,
+  Vector3,
+} from '@babylonjs/core';
 
 export default class Kotatsu extends AbstractMesh {
-
   isLoaded: boolean;
 
   root: AbstractMesh;
@@ -15,42 +23,44 @@ export default class Kotatsu extends AbstractMesh {
   legBR: AbstractMesh;
 
   constructor(public scene: Scene) {
+    super('kotatsu', scene);
 
-    super("kotatsu", scene);
-
-    SceneLoader.ImportMeshAsync("", "./models/", "kotatsu.glb", scene)
+    SceneLoader.ImportMeshAsync('', './models/', 'kotatsu.glb', scene)
       .then((result) => {
         this.root = result.meshes[0];
 
         // Tabletop
-        this.tableTop = new TransformNode("tableTop");
-        const tableTopBase = scene.getMeshByName("tableTopBase");
-        const tableTopPicture = scene.getMeshByName("tableTopPicture");
+        this.tableTop = new TransformNode('tableTop');
+        const tableTopBase = scene.getMeshByName('tableTopBase');
+        const tableTopPicture = scene.getMeshByName('tableTopPicture');
         tableTopBase.parent = this.tableTop;
         tableTopPicture.parent = this.tableTop;
 
         // Futon
-        this.futon = scene.getMeshByName("futon");
+        this.futon = scene.getMeshByName('futon');
 
         // Legs
-        this.legTL = scene.getMeshByName("legTL");
-        this.legTR = scene.getMeshByName("legTR");
-        this.legBL = scene.getMeshByName("legBL");
-        this.legBR = scene.getMeshByName("legBR");
+        this.legTL = scene.getMeshByName('legTL');
+        this.legTR = scene.getMeshByName('legTR');
+        this.legBL = scene.getMeshByName('legBL');
+        this.legBR = scene.getMeshByName('legBR');
 
         // Heater
-        this.heater = scene.getMeshByName("heater");
-        const heaterMaterial = new StandardMaterial("heaterMaterial");
+        this.heater = scene.getMeshByName('heater');
+        const heaterMaterial = new StandardMaterial('heaterMaterial');
         heaterMaterial.emissiveColor = new Color3(1, 0, 0);
         this.heater.material = heaterMaterial;
 
         // Heater Light
-        this.heaterLight = new PointLight("heaterLight", new Vector3(0, 0.3, 0), scene);
+        this.heaterLight = new PointLight(
+          'heaterLight',
+          new Vector3(0, 0.3, 0),
+          scene
+        );
         this.heaterLight.diffuse = new Color3(1, 0, 0);
         this.heaterLight.intensity = 1;
         this.heaterLight.parent = this.root;
-
       })
-      .catch((console.error));
+      .catch(console.error);
   }
 }
